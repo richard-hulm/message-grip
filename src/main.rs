793cbab1c9f9c9e39483f72d6dab9c8bf1495cf4
Oscar Barlow <oscar.barlow@gmail.com>
2019-02-19 19:11:58 +0000

    take command line options and 'list' subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand() {
+    	("list", Some(_sub_args)) => println!("listing queues..."),
+    	_ => panic!("Unexpected command")
+    }
 }
